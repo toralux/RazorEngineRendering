d3b1888ac80de7cbc1a948cdd8059854cac55e24
Tor Anders Johansen <tor.anders.johansen@komplettbank.no>
2020-02-10 10:19:58 +0100

    Applying fix that solved the problem for 3.x (dotnet/aspnetcore/issues/17655)

--- dotnetcore3.1/Rendering.Tests/ComponentTestServerFixture.cs
+++ dotnetcore3.1/Rendering.Tests/ComponentTestServerFixture.cs
@@ -20,6 +20,12 @@ namespace Rendering.Tests
         protected override IWebHostBuilder CreateWebHostBuilder()
         {
             var hostBuilder = new WebHostBuilder();
+            // Solution fixing the problem:
+            // https://github.com/dotnet/aspnetcore/issues/17655#issuecomment-581418168
+            hostBuilder.ConfigureAppConfiguration((context, b) =>
+            {
+                context.HostingEnvironment.ApplicationName = typeof(ViewRender).Assembly.GetName().Name;
+            });
             return hostBuilder.UseStartup<ComponentTestStartup>();
         }
 
